0b60a02ff9d0620ddd192d8d464e04863d6795ad
ycosilvallana <rsilvallana@teko.ph>
2023-05-21 09:17:36 +0800

    Initial commit

---
+++ buildSrc/src/main/java/Libs.kt
@@ -0,0 +1,10 @@
+object Libs {
+    const val appCompat = "androidx.appcompat:appcompat:${Versions.appCompatVersion}"
+    const val cardView = "androidx.cardview:cardview:${Versions.cardViewVersion}"
+    const val recyclerView = "androidx.recyclerview:recyclerview:${Versions.recyclerViewVersion}"
+    const val constraintLayout = "androidx.constraintlayout:constraintlayout:${Versions.constraintLayoutVersion}"
+    const val supportDesign = "com.google.android.material:material:${Versions.materialDesignVersion}"
+    const val coordinatorLayout = "androidx.coordinatorlayout:coordinatorlayout:${Versions.coordinatorLayoutVersion}"
+    const val activityKtx = "androidx.activity:activity-ktx:${Versions.activityVersion}"
+    const val fragmentKtx = "androidx.fragment:fragment-ktx:${Versions.fragmentVersion}"
+}
---
+++ buildSrc/src/main/java/Versions.kt
@@ -0,0 +1,22 @@
+object Versions {
+    // region BuildLibs
+    const val kotlinVersion = "1.7.10"
+    const val androidPluginVersion = "7.3.0"
+    const val googlePlayVersion = "4.3.8"
+    const val crashlyticsGradlePluginVersion = "2.8.1"
+    const val navigationPluginVersion = "2.5.3"
+    // endregion
+
+    // region Libs
+    const val appCompatVersion = "1.3.0"
+    const val cardViewVersion = "1.0.0"
+    const val recyclerViewVersion = "1.2.1"
+    const val constraintLayoutVersion = "2.0.4"
+    const val materialDesignVersion = "1.4.0"
+    const val preferenceManagerVersion = "1.1.1"
+    const val coordinatorLayoutVersion = "1.1.0"
+    const val activityVersion = "1.2.0-beta01"
+    const val fragmentVersion = "1.3.0-beta01"
+    const val firebaseBoMVersion = "28.1.0"
+    // endregion
+}
